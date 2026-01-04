8a0ddd251b97236a9b828fea7b90c0e886435bb4
sakshibora365 <sakshibora536@gmail.com>
2026-01-04 23:52:44 +0530

    swap function

--- day 2 hello.c
+++ day 2 hello.c
@@ -7,8 +7,8 @@ swap(4,6);
 int swap(int a,int b){
     int temp;
   //a ki value hamm 2 assign krh hai its for(a=b)
-  a=temp;
-  b=a;
-  temp=b;
+  temp=a;
+  a=b;//b khallli
+  b=temp;
   printf(" swapping of a=%d and b=%d",&a,&b);  
 }
