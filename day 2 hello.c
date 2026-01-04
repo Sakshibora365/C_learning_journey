4733e47ef4014cbef101c799ebb07fbcea0588e4
sakshibora365 <sakshibora536@gmail.com>
2026-01-04 23:39:53 +0530

    day 2 h.c

--- day 2 hello.c
+++ day 2 hello.c
@@ -1,1 +1,14 @@
-
+//making swap function 
+#include<stdio.h>
+int swap( int a,int b);
+int main(){
+swap(4,6);
+}
+int swap(int a,int b){
+    int temp;
+  //a ki value hamm 2 assign krh hai its for(a=b)
+  a=temp;
+  b=a;
+  temp=b;
+  printf(" swapping of a=%d and b=%d",&a,&b);  
+}
